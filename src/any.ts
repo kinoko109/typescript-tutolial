e02817b34b7d371d3e98c633e768d2b32a700ada
松岡龍紀 <tatsuaki_matsuoka@bita.jp>
2020-09-22 17:40:11 +0900

    fix: any型

---
+++ src/any.ts
@@ -0,0 +1,16 @@
+import axios from "axios";
+
+export {};
+
+let url: string = "https://udemy-utils.herokuapp.com/api/v1/articles?token=token123";
+
+axios.get(url).then(res => {
+  interface Article {
+    id: number;
+    title: string;
+    description: string;
+  }
+  let data: any; // Article[];
+  data = res.data;
+  console.log(data);
+});
