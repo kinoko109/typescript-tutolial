fe218579b26df7e244ec1eef7b19489ca86ee823
松岡龍紀 <tatsuaki_matsuoka@bita.jp>
2020-09-27 18:53:10 +0900

    fix: 型の互換性セクション

---
+++ src/type-compatibility.ts
@@ -0,0 +1,41 @@
+export {};
+
+// 型の互換性セクション
+
+// 互換性あり
+let foo: any;
+let bar: string = "typescript";
+
+console.log(typeof foo); // undefined
+
+// fooにbarを代入できる→型の互換性がある
+foo = bar;
+
+console.log(typeof foo); // string
+
+// -------------------------------------------------
+
+// 互換性なし
+let hoge: string;
+let fuga: number = 1;
+
+// エラー
+// hoge = fuga;
+
+// リテラルの場合は互換性あり
+let fooStringLiteral: "fooStringLiteral" = "fooStringLiteral";
+hoge = fooStringLiteral;
+
+
+// 型の互換性あり
+interface Animal {
+  // age: number;
+}
+
+class Person {
+  constructor(public age: number) {}
+}
+
+let me: Animal;
+
+me = new Person(24);
